501e05c457afb225186163c8429a805aa5e720bd
b2r2 <b2r2@bk.ru>
2018-05-30 23:42:09 +0300

    Implemented parsing and some variables were renamed.

--- main.go
+++ main.go
@@ -5,15 +5,15 @@ import (
 	"os"
 	"projects/Web-Scraper/config"
 	"projects/Web-Scraper/utils"
+	"time"
 
 	"github.com/gocolly/colly"
 	"github.com/gocolly/colly/proxy"
 )
 
-var pattern map[string][]string = map[string][]string{
-	"medium":  {"pre", "figure"},
-	"telegra": {"pre", "figure"},
-	"zen":     {"figure"},
+var patterns map[string][]string = map[string][]string{
+	"medium":  {"section", "div"},
+	"telegra": {"main", "header", "article"},
 }
 
 func main() {
@@ -21,16 +21,33 @@ func main() {
 		log.Fatalf("Missing URL argument. Usage main.go http[s]://web-site...")
 	}
 	urlPath := os.Args[1]
-	domain := utils.GetDomain(urlPath)
+	nameSite := utils.GetDomain(urlPath)
 
 	c := colly.NewCollector(colly.AllowURLRevisit())
-
-	if domain == "telegra" {
+	// set proxy
+	if nameSite == "telegra" {
 		rp, err := proxy.RoundRobinProxySwitcher(config.Proxies...)
 		if err != nil {
 			log.Fatalf("Error when installing proxy ", err)
 		}
 		c.SetProxyFunc(rp)
 	}
+	// parsing content
+	var contentPage []string
+	var querySelector = patterns[nameSite][0]
+	c.OnHTML(querySelector, func(e *colly.HTMLElement) {
+		var temp string
+		var tags []string = patterns[nameSite][1:]
+		for _, tag := range tags {
+			temp = e.ChildText(tag)
+			contentPage = append(contentPage, temp)
+		}
+	})
+	// set parallelism
+	c.Limit(&colly.LimitRule{
+		Parallelism: 2,
+		RandomDelay: 5 * time.Second,
+	})
 	c.Visit(urlPath)
+	c.Wait()
 }
